b1027f73580dd181eb1655f89a96aeda73d774cf
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-03-29 10:43:51 -0300

    Teste dnv

--- getit/settings.py
+++ getit/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,19 +75,19 @@ WSGI_APPLICATION = "getit.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default="postgresql://localhost/getit?user=getituser&password=getitsenha",
-#         conn_max_age=600,
-#         ssl_require=not DEBUG,
-#     )
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://localhost/getit?user=getituser&password=getitsenha",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
