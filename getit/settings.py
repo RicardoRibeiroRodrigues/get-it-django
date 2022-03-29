f86e063ecd1f2055f697988a3fcc9a39af08a31d
Ricardo Ribeiro Rodrigues <riywar@gmail.com>
2022-03-29 11:14:11 -0300

    teste com configs handout

--- getit/settings.py
+++ getit/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dk(k0d2qdiw^=-z961*ewm5)&!am948f1=lha+49g3-v%bzh^@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["aqueous-hamlet-29504.herokuapp.com", "localhost", "127.0.0.1"]
 
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -75,19 +75,19 @@ WSGI_APPLICATION = "getit.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default="postgresql://localhost/getit?user=getituser&password=getitsenha",
-#         conn_max_age=600,
-#         ssl_require=not DEBUG,
-#     )
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://localhost/getit?user=getituser&password=getitsenha",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
