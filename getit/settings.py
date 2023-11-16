3ce665099993027dd9c7f148c44d926017b90406
RicardoRibeiroRodrigues <ricardorrodrigues12@gmail.com>
2023-11-16 16:24:11 -0300

    Static folder

--- getit/settings.py
+++ getit/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
+import os
 
 import dj_database_url
 
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dk(k0d2qdiw^=-z961*ewm5)&!am948f1=lha+49g3-v%bzh^@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["aqueous-hamlet-29504.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["aqueous-hamlet-29504.herokuapp.com", "localhost", "127.0.0.1", "local_getit.com"]
 
 
 # Application definition
@@ -124,8 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
